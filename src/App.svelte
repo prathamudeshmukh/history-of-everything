8ac951eb58f7254984cc1ad9b1f6a7b038658a66
prathamesh <prathamudeshmukh@gmail.com>
2020-03-10 11:58:50 +0530

    Added timeline component

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+	import Timeline from './components/Timeline.svelte';
+	export let timelineData;
+</script>
+<style >
+	body {
+		overflow-x: hidden;
+		font-size: 17px;
+		font-weight: 400;
+		background-color: #222C32;
+	}
+
+	h1 {
+		font-size: 200%;
+		letter-spacing: 3px;
+		font-weight: 400;
+		font-family: 'Cardo', serif;
+		color: white;
+	}
+
+	header {
+		background: #2B343A;
+		color: 'white';
+		padding: 150px 0;
+	}
+</style>
+<body>
+	<header>
+		<div class="container text-center">
+			<h1>History of everything</h1>
+		</div>
+	</header>
+	<Timeline timelineData={timelineData}/>
+</body>
+
---
+++ src/components/Timeline.svelte
@@ -0,0 +1,25 @@
+<script>
+	export let timelineData;
+</script>
+<head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Cardo&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Bree+Serif&display=swap" rel="stylesheet">
+</head>
+<style  src="./timeline.scss"></style>
+
+<section class="timeline">
+  <div class="container">
+	{#each timelineData as event}
+		<div class="timeline-item">
+		<div class="timeline-img"></div>
+		<div class="timeline-content js--fadeInLeft">
+			<h2>Event</h2>
+			<div class="date">{event.event_date}</div>
+			<p>{event.description}</p>
+			<a class="bnt-more" href="javascript:void(0)">More</a>
+		</div>
+		</div>
+	{/each}
+  </div>
+</section>
